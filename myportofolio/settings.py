3c9349d95b4a4ad5fc899494b75385eda05c7c4c
Filza Shafira <filza.shafira@ui.ac.id>
2026-09-13 23:29:21 +0700

    Fix database configuration

--- myportofolio/settings.py
+++ myportofolio/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
 from pathlib import Path
-
 import os
 from dotenv import load_dotenv
 
@@ -81,37 +80,12 @@ WSGI_APPLICATION = 'myportofolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
-if PRODUCTION:
-    # Cek apakah DATABASE_URL atau DB_HOST ada di env PWS
-    if os.getenv('DB_HOST'):
-        DATABASES = {
-            'default': {
-                'ENGINE': 'django.db.backends.postgresql',
-                'NAME': os.getenv('DB_NAME'),
-                'USER': os.getenv('DB_USER'),
-                'PASSWORD': os.getenv('DB_PASSWORD'),
-                'HOST': os.getenv('DB_HOST'),
-                'PORT': os.getenv('DB_PORT', '5432'),
-                'OPTIONS': {
-                    'options': f"-c search_path={os.getenv('SCHEMA', 'public')}"
-                }
-            }
-        }
-    else:
-        # Fallback jika environment DB di PWS belum di-set
-        DATABASES = {
-            'default': {
-                'ENGINE': 'django.db.backends.sqlite3',
-                'NAME': BASE_DIR / 'db.sqlite3',
-            }
-        }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
 
 
 # Password validation
