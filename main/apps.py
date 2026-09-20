4eb1a7b2f2fe0bb5637b43a550dd6fd8db25772c
Filza Shafira <filza.shafira@ui.ac.id>
2026-09-20 16:56:33 +0700

    Add auto create superuser script for PWS

--- main/apps.py
+++ main/apps.py
@@ -17,3 +17,16 @@ class MainConfig(AppConfig):
                     call_command('loaddata', 'experience.json')
             except Exception:
                 pass
+
+class MainConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'main'
+
+    def ready(self):
+        # Jalankan otomatis saat aplikasi Django start di server PWS
+        if 'gunicorn' in sys.argv or 'wsgi' in sys.argv:
+            from django.core.management import call_command
+            try:
+                call_command('create_pws_superuser')
+            except Exception:
+                pass
